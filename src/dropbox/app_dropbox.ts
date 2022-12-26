3d66aef8688136ef2f5ae1026095095c3842f6aa
piechota <piechota33@gmail.com>
2022-12-26 18:47:35 +0100

    production server app

--- src/dropbox/app_dropbox.ts
+++ src/dropbox/app_dropbox.ts
@@ -1,2 +1,5 @@
-export const REDIRECT_URI = 'http://localhost:5173/'; //you need this for redirection, otherwise it just shows Dropbox page with key that you need to enter in app
-export const CLIENT_ID = 'n5f5arc8a4aa3fq'; //app key
+//export const REDIRECT_URI = 'http://localhost:5173/'; //you need this for redirection, otherwise it just shows Dropbox page with key that you need to enter in app
+//export const CLIENT_ID = 'n5f5arc8a4aa3fq'; //app key
+
+export const REDIRECT_URI = 'https://reactionsvault.github.io/';
+export const CLIENT_ID = 'x9vizg3tkvrrleu';
